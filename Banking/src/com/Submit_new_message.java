ac24ccecbfe49020e07fb3cff6156879d7db5f28
IBM <IBM@IBM-Testing05>
2018-07-09 17:09:24 +0530

    srilatha code

---
+++ Banking/src/com/Submit_new_message.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import com.messages.Message_Testng;
+
+public class Submit_new_message extends Message_Testng {
+  @Test(priority=1)
+  public void submit(WebDriver driver) {
+
+	 
+  }
+}
